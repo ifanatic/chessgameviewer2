5040a050f503aeaa4c139effdd3a0f2f931faea5
ifanatic <onlk@yandex.ru>
2012-04-01 14:31:58 +0600

    Simple app

---
+++ src/main/java/app/ChessGameViewerApp.java
@@ -0,0 +1,13 @@
+package app;
+
+/**
+ * chessgameviewer project
+ * User: ifanatic
+ * Date: 01.04.12
+ * Time: 14:27
+ */
+public class ChessGameViewerApp {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
